ce4c5cc8128a9659c4bb41842518f72f43b3d4eb
Laurent <magrez0laurent@gmail.com>
2022-02-17 20:48:58 +0100

    finished js for map & trajets, everything work

--- js/trajets.js
+++ js/trajets.js
@@ -1,49 +1,73 @@
 "use strict";
 
-// can also do : (need to add: id="mapwithbutton" to the 'main container')
-// document.getElementById("mapwithbutton").addEventListener("click", funcPrincipalChange);
+// Add Listener
+const whereToAddListener = document.querySelectorAll(".js-buttons-container");
+for (let elementToListen of whereToAddListener) {
+  elementToListen.addEventListener("click", funcPrincipalChange);
+}
 
-// array so need to get the key.
-// can also use :
-// document.querySelectorAll('.js-buttons-container')[0];
-// Or
-// let mainMap = document.getElementsByClassName('js-buttons-map')[0];
-// mainMap.addEventListener("click", funcPrincipalChange);
-document.querySelector(".js-buttons-container").addEventListener("click", funcPrincipalChange);
-
-/* ---------------------------- */
-const buttonNodeList = document.querySelectorAll(".general__button");
-// All buttons
-
-const imgToChange = document.querySelectorAll(".js-trajets__img");
-const hToChange = document.querySelectorAll(".js-trajets__h2");
-const pToChange = document.querySelectorAll(".js-trajets__p");
-//list of all img, h2, p element to change content from,
-// select them with nodelist index
-/* ---------------------------- */
-
-
-// CONTENT------------------------------------------------------------------
+// CONTENT___________________________________________________________________________
 let imgTrajetsArray = [
   "img/trajets/temp_isole-madagascar_d.jpg.image.1296.974.high.jpg",
   "img/trajets/temp_madagascar_d.jpg.image.1296.974.high.webp",
   "img/trajets/temp_1-nosy-be-madagascar-timbuktu-travel-10.avif",
+  "img/trajets/temp_madagascar_d.jpg.image.1296.974.high.webp",
+  "img/trajets/temp_General-andasibe-madagascar-timbuktu-travel-5.avif",
+  "img/bungalowanakao2.jpg",
+  "img/trajets/temp_1-nosy-be-madagascar-timbuktu-travel-9.avif",
+  "img/trajets/temp_Black-and-white-ruffed-lemur-andasibe-madagascar-timbuktu-travel.avif",
+  "img/trajets/temp_1-nosy-be-madagascar-timbuktu-travel-3.avif",
+  "img/trajets/temp_madagascar_viaggi_sub.jpg",
 ];
 
-let hTrajetsArray = ["Tulear", "St Augustin", "Anakao"];
+let hTrajetsArray = [
+  "Tulear",
+  "St Augustin",
+  "Anakao",
+  "Ifaty",
+  "Salary bay",
+  "Andavadoaka",
+  "Ambola",
+  "Tsimanampetsotsa",
+  "Nosy Ve",
+  "Nosy Satrana",
+];
 
 let pTrajetsArray = [
   "TLorem ipsum, dolor sit amet consectetur adipisicing elit. Minima rerum voluptatum iste nisi veritatis vel debitis perspiciatis cum, vitae totam! hic suscipit repellat aperiam explicabo",
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis?",
   "Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis nulla harum officia? Eaque, pariatur",
+  "TLorem ipsum, dolor sit amet consectetur adipisicing elit. Minima rerum voluptatum iste nisi veritatis vel debitis perspiciatis cum, vitae totam! hic suscipit repellat aperiam explicabo",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis?",
+  "Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis nulla harum officia? Eaque, pariatur",
+  "TLorem ipsum, dolor sit amet consectetur adipisicing elit. Minima rerum voluptatum iste nisi veritatis vel debitis perspiciatis cum, vitae totam! hic suscipit repellat aperiam explicabo",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis?",
+  "Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis nulla harum officia? Eaque, pariatur",
+  "Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis nulla harum officia? Eaque, pariatur",
 ];
 
-// FUNCTION : ANNEXE------------------------------------------------------------------
-// add effect on clicked button and remove it from others if they have it
+// VARIABLES___________________________________________________________________________
+
+// All buttons
+const buttonNodeList = document.querySelectorAll(".general__button");
+
+//Nodelist of all img, h2, p element to change content
+const imgToChange = document.querySelectorAll(".js-trajets__img");
+const hToChange = document.querySelectorAll(".js-trajets__h2");
+const pToChange = document.querySelectorAll(".js-trajets__p");
+/* ---------------------------- */
+let contentToChangeIndex = -1;
+let loopEffectStart = -1;
+let loopEffectLimit = -1;
+let stop = -1;
+
+
+// FUNCTION : ANNEXE___________________________________________________________________________
+// add & remove button on click effect
 function addAndRemoveEffect(clickedButton, buttonClassToSkip, classToAddAndRemove) {
   clickedButton.classList.add(classToAddAndRemove);
 
-  for (let i = 0; i < buttonNodeList.length; i++) {
+  for (let i = loopEffectStart; i <= loopEffectLimit; i++) {
     if (buttonNodeList[i].classList.contains(buttonClassToSkip)) {
       continue;
     }
@@ -53,45 +77,52 @@ function addAndRemoveEffect(clickedButton, buttonClassToSkip, classToAddAndRemov
   }
 }
 /* ---------------------------- */
-function fadeInAndOutChangeContent(fadeclass, eIndexNum, cIndexNum, classSelector) {
-  let articleToFade = document.querySelector(classSelector);
-  articleToFade.classList.add(fadeclass);
-  // articleToFade.style.opacity = 0.1;
+// fade out - change content - fade in
+function fadeInAndOutChangeContent(fadingClass, eIndexNum, cIndexNum, indexClassToFade) {
+  let articleToFade = document.querySelectorAll(".js-principal-article")[indexClassToFade];
+
+  articleToFade.classList.add(fadingClass);
 
   setTimeout(function () {
     imgToChange[eIndexNum].src = imgTrajetsArray[cIndexNum];
     hToChange[eIndexNum].innerHTML = hTrajetsArray[cIndexNum];
     pToChange[eIndexNum].innerHTML = pTrajetsArray[cIndexNum];
-    articleToFade.classList.remove(fadeclass);
-    // articleToFade.style.opacity = 1;
+    articleToFade.classList.remove(fadingClass);
   }, 100);
 }
 
-// FUNCTION : MAIN ------------------------------------------------------------------
-let stop = -1;
-
+// FUNCTION : MAIN___________________________________________________________________________
 function funcPrincipalChange(paramFunc) {
   let whichButton = paramFunc.target;
 
-  let button0 = whichButton.classList.contains("button0-position");
-  let button1 = whichButton.classList.contains("button1-position");
-  let button2 = whichButton.classList.contains("button2-position");
+  let buttonNumber = Number(whichButton.classList.value.match(/[0-9]/));
+  let CurrentButtonClass = whichButton.classList.value.split(" ")[1];
 
   if (whichButton.tagName == "BUTTON") {
-    if (button0 && stop != 0) {
-      stop = 0;
-      addAndRemoveEffect(whichButton, "button0-position", "button-on-click");
-      fadeInAndOutChangeContent("trajet__article-fade", 0, 0, ".js-principal-article");
+    //map1
+    if (buttonNumber >= 0 && buttonNumber <= 2) {
+      contentToChangeIndex = 0;
+      loopEffectStart = 0;
+      loopEffectLimit = 2;
     }
-    if (button1 && stop != 1) {
-      stop = 1;
-      addAndRemoveEffect(whichButton, "button1-position", "button-on-click");
-      fadeInAndOutChangeContent("trajet__article-fade", 0, 1, ".js-principal-article");
+    //map2
+    if (buttonNumber >= 3 && buttonNumber <= 5) {
+      contentToChangeIndex = 1;
+      loopEffectStart = 3;
+      loopEffectLimit = 5;
     }
-    if (button2 && stop != 2) {
-      stop = 2;
-      addAndRemoveEffect(whichButton, "button2-position", "button-on-click");
-      fadeInAndOutChangeContent("trajet__article-fade", 0, 2, ".js-principal-article");
+    //map3
+    if (buttonNumber >= 6 && buttonNumber <= 9) {
+      contentToChangeIndex = 2;
+      loopEffectStart = 6;
+      loopEffectLimit = 9;
+    }
+
+    // Apply all change
+    if (stop != buttonNumber) {
+      stop = buttonNumber;
+      addAndRemoveEffect(whichButton, CurrentButtonClass, "button-on-click");
+      fadeInAndOutChangeContent("trajet__article-fade", contentToChangeIndex, buttonNumber, contentToChangeIndex);
     }
   }
 }
