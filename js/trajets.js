e2ffd699c4ec186d0f8cda9650646bd91be68732
Laurent <magrez0laurent@gmail.com>
2022-02-24 18:58:58 +0100

    map js fixed, do not apply effect any more if go back to a preview map

--- js/trajets.js
+++ js/trajets.js
@@ -1,11 +1,5 @@
 "use strict";
 
-// Add Listener
-const whereToAddListener = document.querySelectorAll(".js-buttons-container");
-for (let elementToListen of whereToAddListener) {
-  elementToListen.addEventListener("click", funcPrincipalChange);
-}
-
 // CONTENT___________________________________________________________________________
 let imgTrajetsArray = [
   "img/trajets/temp_isole-madagascar_d.jpg.image.1296.974.high.jpg",
@@ -46,28 +40,36 @@ let pTrajetsArray = [
   "Id possimus asperiores saepe recusandae hic suscipit repellat aperiam explicabo dolores minima. Sapiente, eos! Architecto facilis nulla harum officia? Eaque, pariatur",
 ];
 
+// Add Listener
+const whereToAddListener = document.querySelectorAll(".js-buttons-container");
+for (let elementToListen of whereToAddListener) {
+  elementToListen.addEventListener("click", funcPrincipalChange);
+}
+
 // VARIABLES___________________________________________________________________________
 
 // All buttons
 const buttonNodeList = document.querySelectorAll(".general__button");
 
+const articleToFade = document.querySelectorAll(".js-trajets-article");
 //Nodelist of all img, h2, p element to change content
-const imgToChange = document.querySelectorAll(".js-trajets__img");
-const hToChange = document.querySelectorAll(".js-trajets__h2");
-const pToChange = document.querySelectorAll(".js-trajets__p");
+const imgToChange = document.querySelectorAll(".js-trajets-article__img");
+const hToChange = document.querySelectorAll(".js-trajets-article__h2");
+const pToChange = document.querySelectorAll(".js-trajets-article__p");
+
+let elemIndexForContentChange = -1;
 /* ---------------------------- */
-let contentToChangeIndex = -1;
 let loopEffectStart = -1;
 let loopEffectLimit = -1;
 let stop = -1;
 
 // FUNCTION : ANNEXE___________________________________________________________________________
 // add & remove button on click effect
-function addAndRemoveEffect(clickedButton, buttonClassToSkip, classToAddAndRemove) {
+function addAndRemoveEffect(clickedButton, classToAddAndRemove) {
   clickedButton.classList.add(classToAddAndRemove);
 
   for (let i = loopEffectStart; i <= loopEffectLimit; i++) {
-    if (buttonNodeList[i].classList.contains(buttonClassToSkip)) {
+    if (buttonNodeList[i] == clickedButton) {
       continue;
     }
     if (buttonNodeList[i].classList.contains(classToAddAndRemove)) {
@@ -77,54 +79,68 @@ function addAndRemoveEffect(clickedButton, buttonClassToSkip, classToAddAndRemov
 }
 /* ---------------------------- */
 // fade out - change content - fade in
-function fadeInAndOutChangeContent(fadingClass, eIndexNum, cIndexNum, indexClassToFade) {
-  let articleToFade = document.querySelectorAll(".js-principal-article")[indexClassToFade];
-
-  articleToFade.classList.add(fadingClass);
+function fadeInAndOutChangeContent(fadingClass, elementIndex, contentIndex) {
+  articleToFade[elementIndex].classList.add(fadingClass);
 
   setTimeout(function () {
-    imgToChange[eIndexNum].src = imgTrajetsArray[cIndexNum];
-    hToChange[eIndexNum].innerHTML = hTrajetsArray[cIndexNum];
-    pToChange[eIndexNum].innerHTML = pTrajetsArray[cIndexNum];
-    articleToFade.classList.remove(fadingClass);
+    imgToChange[elementIndex].src = imgTrajetsArray[contentIndex];
+    hToChange[elementIndex].innerHTML = hTrajetsArray[contentIndex];
+    pToChange[elementIndex].innerHTML = pTrajetsArray[contentIndex];
+
+    articleToFade[elementIndex].classList.remove(fadingClass);
   }, 100);
 }
-
 
 // FUNCTION : MAIN___________________________________________________________________________
 function funcPrincipalChange(paramFunc) {
   let whichButton = paramFunc.target;
 
-  let buttonNumber = whichButton.classList.value.match(/[0-9]/)[0];
-  let whichButtonClass = whichButton.classList.value.split(" ")[1];
+  let buttonNumber = Number(whichButton.classList.value.match(/[0-9]/)[0]);
 
   if (whichButton.tagName == "BUTTON") {
-    //map1
-    if (buttonNumber >= 0 && buttonNumber <= 2) {
-      contentToChangeIndex = 0;
-      loopEffectStart = 0;
-      loopEffectLimit = 2;
-    }
-    //map2
-    if (buttonNumber >= 3 && buttonNumber <= 5) {
-      contentToChangeIndex = 1;
-      loopEffectStart = 3;
-      loopEffectLimit = 5;
-    }
-    //map3
-    if (buttonNumber >= 6 && buttonNumber <= 9) {
-      contentToChangeIndex = 2;
-      loopEffectStart = 6;
-      loopEffectLimit = 9;
-    }
-
     // Apply all change
-    if (stop != buttonNumber) {
+    if (stop != buttonNumber && !whichButton.classList.contains("button-effect-on-click")) {
+      //map1
+      if (buttonNumber >= 0 && buttonNumber <= 2) {
+        elemIndexForContentChange = 0;
+        loopEffectStart = 0;
+        loopEffectLimit = 2;
+      }
+      //map2
+      if (buttonNumber >= 3 && buttonNumber <= 5) {
+        elemIndexForContentChange = 1;
+        loopEffectStart = 3;
+        loopEffectLimit = 5;
+      }
+      //map3
+      if (buttonNumber >= 6 && buttonNumber <= 9) {
+        elemIndexForContentChange = 2;
+        loopEffectStart = 6;
+        loopEffectLimit = 9;
+      }
+
       stop = buttonNumber;
-      addAndRemoveEffect(whichButton, whichButtonClass, "button-on-click");
-      fadeInAndOutChangeContent("trajet__article-fade", contentToChangeIndex, buttonNumber, contentToChangeIndex);
+      addAndRemoveEffect(whichButton, "button-effect-on-click");
+      fadeInAndOutChangeContent("trajet__article-fade", elemIndexForContentChange, buttonNumber);
     }
   }
 
+  // annexeBordelTest();
 }
 
+//___________________________________________________________________________
+function annexeBordelTest() {
+  let buttonMap0NodeList = whereToAddListener[0].querySelectorAll(".general__button");
+
+  // let buttonmap0ToArray = Array.from(buttonMap0NodeList);
+
+  let buttonNumArray = [];
+  buttonMap0NodeList.forEach((nodeListElement) => {
+    let numero = Number(nodeListElement.classList.value.match(/[0-9]/)[0]);
+    buttonNumArray.push(numero);
+
+    // console.log("numero to push into array :", numero);
+  });
+
+  // console.log(buttonNumArray);
+}
